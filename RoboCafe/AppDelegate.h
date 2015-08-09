2381484dcc4e7d3d2ac85f38b7ba530c04a08ff8
Patrick Lazik <dermeister@Patricks-MacBook-Pro.local>
2015-08-09 00:35:55 -0400

    Initial Commit

---
+++ RoboCafe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RoboCafe
+//
+//  Created by Patrick Lazik on 8/9/15.
+//  Copyright (c) 2015 Carnegie Mellon University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RoboCafe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RoboCafe
+//
+//  Created by Patrick Lazik on 8/9/15.
+//  Copyright (c) 2015 Carnegie Mellon University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
